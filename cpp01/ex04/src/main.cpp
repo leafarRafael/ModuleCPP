248c869e05902592b4852c22f89e757f0213a72a
Rafael Butzke <leafarbutzke@hotmail.com>
2024-07-17 19:17:35 -0300

    ex04 finalizando

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbutzke <rbutzke@student.42so.org.br>      +#+  +:+       +#+        */
+/*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:00:38 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/07/17 16:08:56 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/07/17 18:38:53 by rbutzke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,11 @@ int main(int argc, char **argv)
 	str = cpyFile(input[0].c_str());
 	newFile = input[0] + ".replace";
 	std::ofstream outfile(newFile.c_str());
-	replace(str, input[1], input[2]);
 	if (input[1] == input[2])
 		outfile << str;
+	else
+		replace(str, input[1], input[2]);
+	outfile << str;
 	outfile.close();
 	return 0;
 }
@@ -66,14 +68,13 @@ std::string cpyFile(const char *input)
 
 void	replace(std::string & file, std::string orin, std::string newstr)
 {
-	std::cout << file.find(orin);
+	size_t reFind;
 
-	if (file.find(orin) == std::string::npos)
-		std::cout << "DEU RUIM" << '\n';
-/* 	if (file.find(orin))
-	{
-		file.replace(file.find(orin), orin.length(), newstr);
-	}
-	std::cout << file << '\n'; */
-	//replace(file, orin, newstr);
+	reFind = file.find(orin);
+	if (reFind == std::string::npos)
+		return ;
+	file.erase(reFind, orin.length());
+	file.insert(reFind, newstr);
+	//file.replace(file.find(orin), orin.length(), newstr);
+	replace(file, orin, newstr);
 }
